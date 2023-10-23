508c224c482c0f9e611513c2268490a5a281cd1d
Rahisbel <rh@plusteam.tech>
2023-10-22 20:23:46 -0400

    feat: :elephant: firebase hosting and api connected

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,7 +1,7 @@
 /*TODO: Variables de entorno*/
 export const enviroment = {
   production: false,
-  baseUrl   : 'http://127.0.0.1:8000', //provisional
+  baseUrl   : 'http://54.174.211.210', //provisional
   key       : '',
 
 };
